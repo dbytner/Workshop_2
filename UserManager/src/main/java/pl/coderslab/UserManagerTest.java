235d888f4a61677b8b6b565f4fae8dc7660a37b3
dbytner <dambyt@wp.pl>
2023-05-19 09:52:55 +0200

    third commit

--- UserManager/src/main/java/pl/coderslab/UserManagerTest.java
+++ UserManager/src/main/java/pl/coderslab/UserManagerTest.java
@@ -1,8 +1,34 @@
 package pl.coderslab;
 
+import pl.coderslab.entity.User;
+import pl.coderslab.entity.UserDAO;
+
 public class UserManagerTest {
 
     public static void main(String[] args) {
+
+        User user_1 = new User("jkowalski@wp.pl", "Jan Kowalski", "kowalski123");
+        User user_2 = new User("pnowak@wp.pl", "Piotr Nowak", "nowak123");
+        User user_3 = new User("k.kowalczyk@onet.pl", "Krystyna Kowalczyk", "kowalczyk56");
+        User user_4 = new User("zboniek@interia.pl", "Zbigniew Boniek", "pilka09");
+        User user_5 = new User("z.jaskula@wp.pl", "Zenon Jaskuła", "jaskula0012");
+        UserDAO userDAO_1 = new UserDAO();
+        //userDAO_1.create(user_1);
+//        userDAO_1.create(user_2);
+//        userDAO_1.create(user_3);
+//        userDAO_1.create(user_4);
+//        userDAO_1.create(user_5);
+        User user = new User();
+        user = userDAO_1.read(4);
+        System.out.println(user.getId() + " " + user.getUserName() + " " + user.getEmail() + " " + user.getPassword());
+
+
+        user = userDAO_1.read(5);
+        user.setUserName("Robert Kubica");
+        user.setEmail("rkubica@wp.pl");
+        user.setPassword("formula1");
+        userDAO_1.update(user);
+
 
     }
 }
--- UserManager/src/main/java/pl/coderslab/entity/UserDAO.java
+++ UserManager/src/main/java/pl/coderslab/entity/UserDAO.java
@@ -1,6 +1,96 @@
 package pl.coderslab.entity;
 
+import org.mindrot.jbcrypt.BCrypt;
+import pl.coderslab.DBUtil;
+
+import java.sql.*;
+
 public class UserDAO {
+
+    private static final String CREATE_USER_QUERY =
+            "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
+
+    private static final String READ_USER_QUERY = "SELECT * FROM users where id = ?";
+
+    private static final String UPDATE_USER_QUERY =
+            "UPDATE users SET email = ?, username = ?, password = ? WHERE id = ?;";
+
+    private static final String DELETE_USER_QUERY = "DELETE FROM users where id = ?;";
+
+    public User create(User user) {
+
+        try (Connection conn = DBUtil.connect()) {
+            PreparedStatement statement =
+                    conn.prepareStatement(CREATE_USER_QUERY, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, user.getUserName());
+            statement.setString(2, user.getEmail());
+            statement.setString(3, hashPassword(user.getPassword()));
+            statement.executeUpdate();
+            //Pobieramy wstawiony do bazy identyfikator, a następnie ustawiamy id obiektu user.
+            ResultSet resultSet = statement.getGeneratedKeys();
+            if (resultSet.next()) {
+                user.setId(resultSet.getInt(1));
+            }
+            return user;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public User read(int userId){
+        try (Connection conn = DBUtil.connect()) {
+            PreparedStatement statement = conn.prepareStatement(READ_USER_QUERY);
+            statement.setInt(1, userId);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getInt("id"));
+                user.setEmail(resultSet.getString("email"));
+                user.setUserName(resultSet.getString("username"));
+                user.setPassword(resultSet.getString("password"));
+                return user;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return null;
+    }
+
+    public User update(User user) {
+
+        try (Connection conn = DBUtil.connect()) {
+            PreparedStatement statement =
+                    conn.prepareStatement(UPDATE_USER_QUERY);
+            statement.setString(1, user.getEmail());
+            statement.setString(2, user.getUserName());
+            statement.setString(3, hashPassword(user.getPassword()));
+            statement.setInt(4, user.getId());
+            statement.executeUpdate();
+            return user;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public void delete(int userId){
+        try (Connection conn = DBUtil.connect()) {
+            PreparedStatement statement = conn.prepareStatement(DELETE_USER_QUERY);
+            statement.setInt(1, userId);
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
+    }
+
+    public String hashPassword(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
 
 
 }
